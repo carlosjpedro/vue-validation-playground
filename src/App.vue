fcc05f970d01968314db5f8b8df7276e40d03c98
Carlos Pedro <carlospedr@gmail.com>
2020-10-27 21:47:38 +0100

    user form

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <user-form></user-form>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent } from "vue";
+import UserForm from "./components/UserForm.vue";
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { UserForm },
 });
 </script>
 
---
+++ src/components/UserForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <div class="form-group">
+      <label for="username">Username: </label>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        class="form-control"
+        v-model="username"
+      />
+    </div>
+    <div class="form-group">
+      <button class="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const username = ref<string>();
+    const onSubmit = () =>
+      console.log("submited", { username: username.value });
+    return { username, onSubmit };
+  },
+});
+</script>
