351ac3d1d003e6ad74288a4753d642e773dad96a
Carlos Pedro <carlospedr@gmail.com>
2020-10-27 22:13:49 +0100

    setup form validation

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,38 +1,39 @@
 <template>
-  <form @submit.prevent="onSubmit">
+  <Form v-slot="{ errors }">
     <div class="form-group">
       <label for="username">Username: </label>
-      <validation-provider rules="required" v-slot="{ errors }">
-        <input
-          type="text"
-          name="username"
-          id="username"
-          class="form-control"
-          v-model="username"
-        />
-        <span>{{ errors[0] }}</span>
-      </validation-provider>
+
+      <Field
+        as="input"
+        type="text"
+        name="username"
+        id="username"
+        class="form-control"
+        v-model="username"
+        :rules="isRequired"
+      />
+      <span>{{ errors.username }}</span>
     </div>
     <div class="form-group">
       <button class="btn btn-primary">Submit</button>
     </div>
-  </form>
+  </Form>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import { extend, ValidationProvider } from "vee-validate";
-import { required } from "vee-validate/dist/rules";
-
-extend("required", {...required, message: "ohhh" });
+import { Form, Field } from "vee-validate";
 
 export default defineComponent({
-  components: { ValidationProvider },
+  components: { Form, Field },
   setup() {
     const username = ref<string>();
+
+    const isRequired = (value: unknown) => (value ? true : false);
+
     const onSubmit = () =>
       console.log("submited", { username: username.value });
-    return { username, onSubmit };
+    return { username, onSubmit, isRequired };
   },
 });
 </script>
