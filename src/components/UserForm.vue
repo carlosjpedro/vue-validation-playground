71bbc369e163c61a8f06659de2860bab21b02897
Carlos Pedro <carlospedr@gmail.com>
2020-10-27 21:53:13 +0100

    add validation provider - failing

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -2,13 +2,16 @@
   <form @submit.prevent="onSubmit">
     <div class="form-group">
       <label for="username">Username: </label>
-      <input
-        type="text"
-        name="username"
-        id="username"
-        class="form-control"
-        v-model="username"
-      />
+      <validation-provider rules="required" v-slot="{ errors }">
+        <input
+          type="text"
+          name="username"
+          id="username"
+          class="form-control"
+          v-model="username"
+        />
+        <span>{{ errors[0] }}</span>
+      </validation-provider>
     </div>
     <div class="form-group">
       <button class="btn btn-primary">Submit</button>
@@ -18,8 +21,13 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import { extend, ValidationProvider } from "vee-validate";
+import { required } from "vee-validate/dist/rules";
+
+extend("required", {...required, message: "ohhh" });
 
 export default defineComponent({
+  components: { ValidationProvider },
   setup() {
     const username = ref<string>();
     const onSubmit = () =>
